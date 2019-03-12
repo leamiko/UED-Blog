bd0949ae9194d3b56002276dcd6f6dcf310a5ea1
huihui <wangshaohui@zhongruigroup.com>
2019-03-12 09:32:42 +0800

    测试提交

--- blog-system/src/App.vue
+++ blog-system/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">智投研发</router-link> |
+      <router-link to="/about">金服研发</router-link>
     </div>
     <router-view/>
   </div>
