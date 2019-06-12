9a0321119067a9614e58638eb95ebaff83d1d45e
yuanyongxiao <yuanyongxiao@zhongruigroup.com>
2019-06-12 13:57:53 +0800

    文章页面修改，样式统一

--- blog-system/src/views/Article-management/Article.vue
+++ blog-system/src/views/Article-management/Article.vue
@@ -1,246 +1,307 @@
 <template>
-  <div class="ant-page scroll-content">
-    <div id="components-form-demo-advanced-search">
+  <div>
+    <div class="add_row">
+      <span>文章管理</span>
+      <a-button type="primary" icon="plus" @click="addBug()" >新增</a-button>
+    </div>
+    <!-- 筛选 -->
+    <div
+      class="select_block"
+      id="ant-advanced-search-form"
+    >
       <a-form
         class="ant-advanced-search-form"
         :form="form"
         @submit="handleSearch"
       >
         <a-row :gutter="24">
-          <a-col :span="12">
-            <a-form-item
-              v-bind="formItemLayout"
-              :label="`名称:`"
-            >
+          <a-col
+            v-for="i in fiedls"
+            :key="i.id"
+            :span="7"
+            :style="{ display: i.id < count ? 'block' : 'none' }"
+          >
+            <a-form-item v-if="i.name!=='分类' && i.name!=='精选' && i.name!=='审核'" :label="`${i.name}`">
               <a-input
-                v-decorator="[ `title`, { rules: [{ required: false, message: 'Input something!', }], } ]"
-                placeholder="请输入"
+                v-decorator="[
+                  `${i.field}`,
+                  {
+                    rules: [
+                      {
+                        required: false,
+                        message: 'Input something!'
+                      }
+                    ]
+                  }
+                ]"
+                v-bind:placeholder="'请输入' + i.name"
               />
             </a-form-item>
-          </a-col>
-          <a-col :span="12">
-            <a-form-item
-              v-bind="formItemLayout"
-              :label="`分类：`"
-            >
-              <!-- <a-input v-decorator="[ `blogType`, { rules: [{ required: false, message: 'Input something!', }], } ]" placeholder="请输入"/> -->
-              <a-select
-                v-model="placeholder"
-                showSearch
-                allowClear
-                optionFilterProp="children"
-                style="width: 200px"
-                @focus="handleFocus"
-                @blur="handleBlur"
-                @change="handleChange"
-                :filterOption="filterOption"
-              >
-                <a-select-option
-                  v-for="i in blogTypeArr"
-                  :key="i"
-                >{{i}}</a-select-option>
+            <a-form-item v-if="i.name==='分类'"
+                         :label="`${i.name}`">
+              <a-select mode="multiple"
+                        notFoundContent="无匹配项"
+                        v-bind:placeholder="'请选择' + i.name"
+                        :tokenSeparators="[',']"
+                        @change="handleChange"
+                        v-decorator="[
+            `${i.field}`,
+            {
+                rules: [{
+                required: false, message: '请输入分类!',
+                }]
+            }
+            ]">
+                <a-select-option v-for="item in keywordsData"
+                                 :key="item">
+                  {{item}}
+                </a-select-option>
+              </a-select>
+            </a-form-item>
+            <a-form-item v-if="i.name==='精选'"
+                         :label="`${i.name}`">
+              <a-select v-decorator="[
+                `${i.field}`,
+                {
+                  rules: [{
+                    required: false,
+                    message: 'Input something!',
+                  }],
+                }
+              ]"
+                        style='width: 120px'
+                        v-bind:placeholder="'请选择'">
+                <a-select-option value="false">否</a-select-option>
+                <a-select-option value="true">是</a-select-option>
+              </a-select>
+            </a-form-item>
+            <a-form-item v-if="i.name==='审核'"
+                         :label="`${i.name}`">
+              <a-select v-decorator="[
+                `${i.field}`,
+                {
+                  rules: [{
+                    required: false,
+                    message: 'Input something!',
+                  }],
+                }
+              ]"
+                        style='width: 120px'
+                        v-bind:placeholder="'请选择'">
+                <a-select-option value="false">否</a-select-option>
+                <a-select-option value="true">是</a-select-option>
               </a-select>
             </a-form-item>
           </a-col>
         </a-row>
-        <a-row :gutter="24">
-          <a-col
-            :span="12"
-            :style="{ textAlign: 'left' }"
-          >
-            <a-button
-              type="primary"
-              icon="plus"
-              @click="addArticle()"
-            > 新建 </a-button> &emsp;
-          </a-col>
-          <a-col
-            :span="12"
-            :style="{ textAlign: 'right' }"
-          >
-            <a-button
-              type="primary"
-              html-type="submit"
-            > 查询 </a-button>
-            <a-button
-              :style="{ marginLeft: '8px' }"
-              @click="handleReset"
-            > 重置 </a-button>
-            <!-- <a :style="{ marginLeft: '8px', fontSize: '12px' }" @click="toggle" > {{expand ? '收起' : '展开'}} <a-icon :type="expand ? 'up' : 'down'" /></a> -->
+        <a-row>
+          <a-col :span="24"
+                 :style="{ textAlign: 'right' }">
+            <a-button type="primary"
+                      html-type="submit">
+              查询
+            </a-button>
+            <a-button :style="{ marginLeft: '8px' }"
+                      @click="handleReset">
+              清空
+            </a-button>
+            <a :style="{ marginLeft: '8px', fontSize: '12px' }"
+               @click="toggle">
+              {{expand?'折叠':'展开'}}
+              <a-icon :type="expand ? 'up' : 'down'" />
+            </a>
           </a-col>
         </a-row>
       </a-form>
-      <div class="search-result-list scroll-content">
-        <a-table
-          :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
-          :dataSource="data"
-          :columns="columns"
-          :rowKey:="data.key"
-          :loading="loading"
+    </div>
+    <div class="tables">
+      <!-- <a-input /> -->
+      <a-table
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        :dataSource="data"
+        :columns="columns"
+        :rowKey:="data.key"
+        :locale="{emptyText: '暂无数据'}"
+      >
+        <template
+          slot="operation"
+          slot-scope="text, record"
         >
-          <template
-            slot="operation"
-            slot-scope="text, record"
+          <a
+            class="right_gap"
+            @click="showItems(record.key)"
+          >查看</a>
+          <a
+            class="right_gap"
+            @click="editItems(record)"
+          >编辑</a>
+          <a-popconfirm
+            v-if="data.length"
+            title="确定删除这条信息吗？"
+            okText="确定"
+            cancelText="取消"
+            @confirm="() => onDelete(record.key)"
           >
-            <a @click="showItems(record.key)">查看 </a>
-            <a @click="editItems(record)">编辑 </a>
-            <a-popconfirm
-              v-if="data.length"
-              title="Sure to delete?"
-              @confirm="() => onDelete(record.key)"
-            >
-              <a>删除</a>
-            </a-popconfirm>
-          </template>
-        </a-table>
-      </div>
+            <a>删除</a>
+          </a-popconfirm>
+        </template>
+      </a-table>
     </div>
   </div>
 </template>
-
 <script>
 import moment from 'moment'
 export default {
   data () {
     return {
-      loading: false,
       expand: false,
-      count: 3,
       form: this.$form.createForm(this),
-      formItemLayout: {
-        labelCol: {
-          xs: { span: 24 },
-          sm: { span: 4 }
-        },
-        wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 12 }
-        }
-      },
+      fiedls: [
+        { id: 1, name: '名称', field: 'title' },
+        { id: 2, name: '作者', field: 'author' },
+        { id: 3, name: '分类', field: 'blogType' },
+        { id: 4, name: '精选', field: 'isGood' },
+        { id: 5, name: '审核', field: 'isAudit' }
+      ],
       data: [],
-      blogTypeArr: ['CSS', 'TS', 'VUE', 'JS', 'HTML', '前端'],
-      blogType: '',
-      placeholder: 'Select a blogType',
-      status: [],
+      queryParams: {},
       searchText: '',
       searchInput: null,
       selectedRowKeys: [],
       columns: [{
         title: '名称',
-        dataIndex: 'title',
-        width: 100
+        dataIndex: 'title'
       }, {
         title: '分类',
         dataIndex: 'blogType',
-        width: 100
-      }, {
+        scopedSlots: {
+          customRender: 'blogType'
+        }
+      },
+      {
         title: '作者',
-        dataIndex: 'author',
-        width: 100
+        dataIndex: 'author'
       },
       {
         title: '点赞数',
         dataIndex: 'likeNum',
-        width: 100
+        scopedSlots: {
+          customRender: 'likeNum'
+        }
       },
       {
         title: '精选',
         dataIndex: 'isGood',
-        width: 100
+        scopedSlots: {
+          customRender: 'isGood'
+        }
       },
       {
-        title: '审核通过',
+        title: '审核',
         dataIndex: 'isAudit',
-        width: 100
+        scopedSlots: {
+          customRender: 'isAudit'
+        }
       },
       {
         title: '创建时间',
-        dataIndex: 'createAt',
-        width: 100
+        dataIndex: 'createAt'
       },
       {
         title: '操作',
         dataIndex: 'operation',
         scopedSlots: {
           customRender: 'operation'
-        },
-        width: 100
+        }
       }
-      ]
+      ],
+      keywordsData: []
+    }
+  },
+  mounted () {
+    this.getList()
+    this.getBugKeywords()
+  },
+  computed: {
+    count () {
+      return this.expand ? 6 : 4
     }
   },
   methods: {
     // 下拉框选择分类
-    handleChange (value) {
-      console.log(`selected ${value}`)
-      this.blogType = value
-    },
-    handleBlur () {
-      console.log('blur')
-    },
-    handleFocus () {
-      console.log('focus')
-    },
-    filterOption (input, option) {
-      return option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
-    },
-
-    // 新建
-    addArticle (record) {
-      this.$router.push({
-        name: 'articleAdd'
-      })
-    },
+    // handleChange (value) {
+    //   console.log(`selected ${value}`)
+    //   this.blogType = value
+    // },
+    // handleBlur () {
+    //   console.log('blur')
+    // },
+    // handleFocus () {
+    //   console.log('focus')
+    // },
+    // filterOption (input, option) {
+    //   return option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
+    // },
 
     // 查询
-    handleSearch (e) {
-      e.preventDefault()
-      this.form.validateFields((error, values) => {
-        if (values.title === undefined) {
-          values.title = ''
-        };
-        const params = {
-          paging: {
-            page: 1,
-            limit: 10
-          },
-          filters: {
-            title: values.title,
-            blogType: this.blogType
-          }
-        }
-        this.getList(params)
-      })
-    },
+    // handleSearch (e) {
+    //   e.preventDefault()
+    //   this.form.validateFields((error, values) => {
+    //     if (values.title === undefined) {
+    //       values.title = ''
+    //     };
+    //     const params = {
+    //       paging: {
+    //         page: 1,
+    //         limit: 10
+    //       },
+    //       filters: {
+    //         title: values.title,
+    //         blogType: this.blogType
+    //       }
+    //     }
+    //     this.getList(params)
+    //   })
+    // },
 
     // 重置
-    handleReset () {
-      this.form.resetFields()
-      this.placeholder = 'Select a blogType'
+    // handleReset () {
+    //   this.form.resetFields()
+    //   this.placeholder = 'Select a blogType'
+    //   const params = {
+    //     paging: {
+    //       page: 1,
+    //       limit: 10
+    //     },
+    //     filters: {
+    //       title: '',
+    //       blogType: ''
+    //     }
+    //   }
+    //   this.getList(params)
+    // },
+
+    // 获得blog列表
+    getList: async function () {
       const params = {
         paging: {
           page: 1,
           limit: 10
         },
         filters: {
-          title: '',
-          blogType: ''
+          title: this.queryParams !== {} ? this.queryParams.title: '',
+          author: this.queryParams !== {} ? this.queryParams.author: '',
+          blogType: this.queryParams !== {} ? this.queryParams.blogType: '',
+          isGood: this.queryParams !== {} ? this.queryParams.isGood: '',
+          isAudit: this.queryParams !== {} ? this.queryParams.isAudit: ''
         }
       }
-      this.getList(params)
-    },
-
-    // 获得blog列表
-    getList: async function (params) {
-      this.loading = true
       let url = this.api.blogList
       const res = await this.$http.post(url, params)
       console.log(res)
       if (res.data) {
-        this.loading = false
         this.data = res.data.data
       } else {
-        this.loading = false
         this.$notification['error']({
           message: '错误',
           placement: 'bottomRight',
@@ -251,23 +312,34 @@ export default {
       for (let i = 0; i < this.data.length; i++) {
         this.data[i].key = this.data[i]._id
         // 数据显示形式的转换
-        if (this.data[i].isGood === false) {
-          this.data[i].isGood = '否'
-        } else {
-          this.data[i].isGood = '是'
-        }
-        if (this.data[i].isAudit === false) {
-          this.data[i].isAudit = '否'
-        } else {
-          this.data[i].isAudit = '是'
-        }
-        this.data[i].createAt = moment('2019-04-28T06:54:31.914Z').format('YYYY-MM-DD')
+        this.data[i].isGood = this.data[i].isGood ? '是' : '否'
+        this.data[i].isAudit = this.data[i].isAudit ? '是' : '否'
+        this.data[i].createAt = moment('2019-04-28T06:54:31.914Z').format('YYYY-MM-DD HH:MM')
       }
     },
     onSelectChange (selectedRowKeys) {
       this.selectedRowKeys = selectedRowKeys
     },
-
+    handleSearch (e) {
+      e.preventDefault()
+      this.form.validateFields((error, values) => {
+        this.queryParams = values
+      })
+      this.getList()
+    },
+    handleReset (clearFilters) {
+      this.form.resetFields()
+      this.queryParams = {}
+      this.getList()
+    },
+    handleChange (e) {
+      console.log(e)
+      this.cacheKeywords = e
+    },
+    // 展开折叠查询项
+    toggle () {
+      this.expand = !this.expand
+    },
     // 查看
     showItems (record) {
       this.$router.push({
@@ -277,17 +349,6 @@ export default {
         }
       })
     },
-
-    // 编辑
-    editItems (record) {
-      this.$router.push({
-        name: 'articleAdd',
-        query: {
-          articleInfo: record
-        }
-      })
-    },
-
     // 删除
     onDelete (record) {
       this.deleteBlog(record)
@@ -299,77 +360,12 @@ export default {
         blogId: record
       })
       if (res.status_code === 200) {
-        const params = {
-          paging: {
-            page: 1,
-            limit: 10
-          },
-          filters: {
-            title: '',
-            blogType: ''
-          }
-        }
-        this.getList(params)
+        this.$message.success('删除成功！')
+        this.getList()
+      } else {
+        this.$message.error(res.message)
       }
     }
-  },
-  mounted () {
-    const params = {
-      paging: {
-        page: 1,
-        limit: 10
-      },
-      filters: {
-        title: '',
-        blogType: ''
-      }
-    }
-    this.getList(params)
   }
 }
 </script>
-
-<style lang='scss' scoped>
-::-webkit-scrollbar {
-  width: 6px;
-  height: 2px;
-  background: white;
-  border-radius: 10px;
-}
-
-::-webkit-scrollbar-thumb {
-  display: block;
-  width: 6px;
-  margin: 0 auto;
-  border-radius: 10px;
-  background: #ccc;
-}
-
-.scroll-content {
-  overflow-y: auto;
-}
-
-.ant-page {
-  height: 100%;
-
-  > div {
-    @extend .ant-page;
-  }
-
-  .search-result-list {
-    height: calc(100% - 175px);
-  }
-}
-
-.ant-advanced-search-form {
-  padding: 24px;
-}
-
-.ant-advanced-search-form .ant-form-item {
-  display: flex;
-}
-
-.ant-advanced-search-form .ant-form-item-control-wrapper {
-  flex: 1;
-}
-</style>
