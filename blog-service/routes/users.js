aeeddfd6dcfb6ddb4f838cb5aff2cbfc69761f58
sunshuai <sunshuai.1@zhongruigroup.com>
2019-06-19 10:10:57 +0800

    超级账号

--- blog-service/routes/users.js
+++ blog-service/routes/users.js
@@ -1,16 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var user = require('../controller/user');
+var user = require("../controller/user");
 
 /* GET users listing. */
-router.post('/login', function (req, res, next) {
+router.post("/login", function(req, res, next) {
   //调用controller方法
   user.login(req, res);
 });
 
-router.get('/logOut', function (req, res, next) {
+router.get("/logOut", function(req, res, next) {
   //调用controller方法
   user.logOut(req, res);
+});
+
+//注册超级账号
+router.get("/register", function(req, res) {
+  user.insert(req, res);
 });
 
 module.exports = router;
