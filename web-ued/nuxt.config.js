2d2b11bb2c07b3a9acd11f0ad510da2158337235
chendezhen <chendezhen@zhongruigroup.com>
2019-08-29 18:38:32 +0800

    build
    nuxt.js模板

---
+++ web-ued/nuxt.config.js
@@ -0,0 +1,69 @@
+
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'author', content: process.env.npm_package_author || ''},
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** 环境配置
+  */
+  env: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:8400'
+  },
+  server: {
+    port: 8400, // default: 3000
+    host: 'localhost', // default: localhost,
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^element-ui/],
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    },
+    analyze: {
+      analyzerMode: 'static'
+    }
+  }
+}
---
+++ web-ued/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    {{title}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      title: 'OHMA ZI-O'
+    }
+  },
+  methods() {
+
+  },
+  created() {
+    this.$message({
+      message: '恭喜你，这是一条成功消息',
+      type: 'success'
+    });
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
