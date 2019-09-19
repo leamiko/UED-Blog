6d59f56ce1d79795861e7641530789123e7b75e7
sunshuai <sunshuai.1@zhongruigroup.com>
2019-09-19 10:33:59 +0800

    登录状态

--- web-ued/store/index.js
+++ web-ued/store/index.js
@@ -4,15 +4,15 @@ import * as CryptoJS from 'crypto-js'
 const KEY = CryptoJS.enc.Utf8.parse(process.env.SECRET_KEY)
 const IV = CryptoJS.enc.Utf8.parse(process.env.KEY_OFFSET)
 // cookie字符串 转换成对象
-function cookieparse(cookieOBJ) {
-  let cookieArr = cookieOBJ.split('; ')
-  let obj = {}
-  cookieArr.forEach(i => {
-    let arr = i.split('=')
-    obj[arr[0]] = arr[1]
-  })
-  return obj
-}
+// function cookieparse(cookieOBJ) {
+//   let cookieArr = cookieOBJ.split('; ')
+//   let obj = {}
+//   cookieArr.forEach(i => {
+//     let arr = i.split('=')
+//     obj[arr[0]] = arr[1]
+//   })
+//   return obj
+// }
 
 // 加密算法
 function Encrypt(word) {
@@ -49,16 +49,14 @@ export const mutations = {
 
 export const actions = {
   // nuxtServerInit is called by Nuxt.js before server-rendering every page
-  // nuxtServerInit({ commit }, { req }) {
-  //   let cookie = req.headers.cookie
-  //   // 将cookie转成json对象
-  //   if (cookieparse(cookie).AUTHTOKEN) {
-  //     commit('SET_TOKEN', cookieparse(cookie).AUTHTOKEN)
-  //   }
-  // },
+  nuxtServerInit({ commit }, { req }) {
+    if (req.session.user) {
+      commit('SET_TOKEN', Encrypt(req.session.user.account))
+    }
+  },
   async login({ commit }, data) {
     // 设置cookie缓存，时限为 30d
-    VueCookie.set('AUTHTOKEN', Encrypt(data.account), 60 * 60 * 24 * 15)
+    VueCookie.set('AUTHTOKEN', Encrypt(data.account), 60 * 60 * 24 * 30)
     commit('SET_TOKEN', Encrypt(data.account))
   },
 
