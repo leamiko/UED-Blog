066976581f3c9fbd14b203b532f906d91fd7accf
dulinda <dulinda@zhongruigroup.com>
2019-10-16 18:45:53 +0800

    test

---
+++ .history/web-ued/pages/coding/dldtest_20191016184112.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>杜林达test</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      test: "123"
+    };
+  },
+  mounted() {
+    this.test();
+  },
+  methods: {
+    test() {
+      console.log(this.test);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
