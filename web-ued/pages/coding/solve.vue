3a3144f003a4a428a4b1ad90dde4ec61241b0091
chendezhen <chendezhen@zhongruigroup.com>
2019-10-17 18:46:12 +0800

    更新package。json
    获取后请运行指令：npm install vue-quill-editor --save

--- web-ued/pages/coding/solve.vue
+++ web-ued/pages/coding/solve.vue
@@ -6,9 +6,12 @@
           <my-header activeLabel="打码"></my-header>
         </div>
         <div class="my-content">
-          <el-card class="box-card" header="卡片名称">
-            <div v-for="o in 4" :key="o" class="text item">
-              {{'列表内容 ' + o }}
+          <el-card>
+            <div slot="header">
+              <h5>提供解决方案</h5>
+            </div>
+            <div>
+              <div :content="content" v-quill:myQuillEditor="editorOption" @blur="onEditorBlur($event)" @focus="onEditorFocus($event)" @ready="onEditorReady($event)"></div>
             </div>
           </el-card>
         </div>
@@ -34,13 +37,30 @@ export default {
   data() {
     return {
       searchVal: '',
-      className: 'custom-dialog'
+      className: 'custom-dialog',
+      content: '',
+      editorOption: {
+       // something config
+      }
     }
   },
   methods: {
-    getSearch(val) {
-      console.log(val);
+    onEditorBlur(editor) {
+      console.log('editor blur!', editor)
+    },
+    onEditorFocus(editor) {
+      console.log('editor focus!', editor)
+    },
+    onEditorReady(editor) {
+      console.log('editor ready!', editor)
+    },
+    onEditorChange({ editor, html, text }) {
+      // console.log('editor change!', editor, html, text)
+      this.content = html
     }
+  },
+  mounted() {
+    console.log('app init, my quill insrance object is:', this.myQuillEditor);
   }
 }
 </script>
@@ -55,19 +75,6 @@ export default {
   z-index: 11;
   background: white;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 0px 2px;
-}
-
-.my-search {
-  padding-top: 66px;
-  padding-bottom: 40px;
-  border-top: 1px solid #ECECEC;
-
-  > div {
-    @extend .center;
-    width: 62.5%;
-    min-width: 600px;
-    max-width: 922px;
-  }
 }
 .my-content {
   padding-top: 100px;
