1cfaac9d0adbe93c36f1c61543f3808c2103b979
duml <dumingliang1995@163.com>
2019-10-22 21:53:52 +0800

    评论api

---
+++ web-ued/server/models/comment-bug.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose')
+var db = require('./db.js')
+var Schema = mongoose.Schema
+
+var bugCommentSchema = new Schema({
+    commentName: {
+        type: String //评论人
+    },
+    commentId: {
+        type: String //评论人objectid
+    },
+    bugId: {
+        type: String //条目objectid
+    },
+    likeNum: {
+        type: Number, //点赞数
+        default: 0
+    },
+    content: {
+        type: String //内容
+    }
+}, {
+    timestamps: {
+        createdAt: 'createAt',
+        updatedAt: 'updateAt'
+    }
+})
+
+module.exports = db.model('bugComment', bugCommentSchema, 'bugcomment')
