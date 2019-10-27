7cb9d3a8a86269deabeeb8cbba8549cbaa758c88
dulinda <dulinda@zhongruigroup.com>
2019-10-27 17:51:51 +0800

    解决方案详情评论切图

--- web-ued/pages/coding/detail.vue
+++ web-ued/pages/coding/detail.vue
@@ -1,87 +1,95 @@
 <template>
-  <my-scrollbar hasHead hasFoot :headStyle="{'background':'white'}" :headActive="'打码'">
-    <div slot="container">
+  <div>
+    <my-scrollbar hasHead hasFoot :headStyle="{'background':'white'}" :headActive="'打码'">
+      <div slot="container">
         <div class="detail_container">
-            <div class="support">
-              <div class="support_icon pointer">
-                <img src="@/assets/img/icon/praise_small_icon.svg">
-              </div>
-              <div class="support_text">
-                <span>点赞</span>
-                <span class="separate inline"></span>
-                <span>分享</span>
-              </div>
-              <div class="support_icon pointer">
-                <img src="@/assets/img/icon/wechat_icon.svg">
-              </div>
-              <div class="support_icon pointer">
-                <img src="@/assets/img/icon/wechat_share_icon.svg">
-              </div>
-              <div class="support_icon pointer">
-                <img src="@/assets/img/icon/weibo_icon.svg">
+          <div class="support">
+            <div class="support_icon pointer">
+              <img src="@/assets/img/icon/praise_small_icon.svg" />
+            </div>
+            <div class="support_text">
+              <span>点赞</span>
+              <span class="separate inline"></span>
+              <span>分享</span>
+            </div>
+            <div class="support_icon pointer">
+              <img src="@/assets/img/icon/wechat_icon.svg" />
+            </div>
+            <div class="support_icon pointer">
+              <img src="@/assets/img/icon/wechat_share_icon.svg" />
+            </div>
+            <div class="support_icon pointer">
+              <img src="@/assets/img/icon/weibo_icon.svg" />
+            </div>
+          </div>
+          <div class="detail_info inline bg-white">
+            <div class="detail_title">
+              nodeJs使用md5报错
+              <div class="detail_presenter">
+                <div class="presenter_head flt inline">
+                  <img src="@/assets/img/image/code_presenter.png" />
+                </div>
+                <span class="presenter_info inline">UILEO · 2019-08-28</span>
+                <div class="mark_tags inline">
+                  <span class="mark_tag">javascript</span>
+                  <span class="mark_tag">php</span>
+                  <span class="mark_tag">css</span>
+                </div>
+                <div class="browse inline">
+                  <div class="browse_icon inline">
+                    <img src="@/assets/img/icon/browse.png" />
+                  </div>6713
+                </div>
               </div>
             </div>
-            <div class="detail_info inline bg-white">
-                <div class="detail_title">
-                    nodeJs使用md5报错
-                    <div class="detail_presenter">
-                        <div class="presenter_head flt inline">
-                            <img src="@/assets/img/image/code_presenter.png">
-                        </div>
-                        <span class="presenter_info inline">
-                            UILEO · 2019-08-28
-                        </span>
-                        <div class="mark_tags inline">
-                            <span class="mark_tag">javascript</span>
-                            <span class="mark_tag">php</span>
-                            <span class="mark_tag">css</span>
-                        </div>
-                        <div class="browse inline">
-                            <div class="browse_icon inline">
-                                <img src="@/assets/img/icon/browse.png"> 
-                            </div>
-                            6713
-                        </div>
-                    </div>
+            <div class="detail_content">
+              <div class="detail_sign">
+                <span class="span_sign inline flt"></span>问题描述
+              </div>
+              <div class="describe">
+                <div class="word">nodeJs报错，如图：</div>
+                <div class="image">
+                  <img src="@/assets/img/bg/bg-dialog-ask.png" />
                 </div>
-                <div class="detail_content">
-                    <div class="detail_sign"><span class="span_sign inline flt"></span>问题描述</div>
-                    <div class="describe">
-                      <div class="word">nodeJs报错，如图：</div>
-                      <div class="image">
-                        <img src="@/assets/img/bg/bg-dialog-ask.png">
-                      </div>
-                    </div>
-                </div>
-                <div class="detail_content">
-                    <div class="detail_sign"><span class="span_sign inline flt"></span>解决方案</div>
-                    <div class="describe">
-                      <div class="word">原因是一个crypto实例只能调用digest一次。 <br>解决方法如图：</div>
-                      <div class="image">
-                        <img src="@/assets/img/bg/bg-dialog-ask.png">
-                      </div>
-                    </div>
-                </div>
-                <div class="praise">
-                  <div class="praise_img pointer">
-                    <img src="@/assets/img/icon/praise.png">
-                  </div>
-                  <div class="praise_num">136个赞</div>
-                </div>
+              </div>
             </div>
-            <div class="interest inline frt">
-                <div class="interest_title">
-                    你可能感兴趣
-                    <router-link class="more frt" :to="'/coding/list'">更多<i class="el-icon-arrow-right"></i></router-link>
+            <div class="detail_content">
+              <div class="detail_sign">
+                <span class="span_sign inline flt"></span>解决方案
+              </div>
+              <div class="describe">
+                <div class="word">
+                  原因是一个crypto实例只能调用digest一次。
+                  <br />解决方法如图：
                 </div>
-                <div class="interest_list">
-                    <div class="interest_info pointer">mysql批量插入数据，一次插入多少 行数据效率最高？</div>
-                    <div class="interest_info pointer active">LUNZ+——select选择项为请选择时 表单不验证</div>
-                    <div class="interest_info pointer">Vue框架在ios微信端存在input输入时 系统键盘会将整个浏览器推上去</div>
-                    <div class="interest_info pointer">小程序手机端数字键盘无法输入小数</div>
-                    <div class="interest_info pointer">Java如何实现五分钟内重复获取返回 同一个短信验证码</div>
+                <div class="image">
+                  <img src="@/assets/img/bg/bg-dialog-ask.png" />
                 </div>
+              </div>
             </div>
+            <div class="praise">
+              <div class="praise_img pointer">
+                <img src="@/assets/img/icon/praise.png" />
+              </div>
+              <div class="praise_num">136个赞</div>
+            </div>
+          </div>
+          <div class="interest inline frt">
+            <div class="interest_title">
+              你可能感兴趣
+              <router-link class="more frt" :to="'/coding/list'">
+                更多
+                <i class="el-icon-arrow-right"></i>
+              </router-link>
+            </div>
+            <div class="interest_list">
+              <div class="interest_info pointer">mysql批量插入数据，一次插入多少 行数据效率最高？</div>
+              <div class="interest_info pointer active">LUNZ+——select选择项为请选择时 表单不验证</div>
+              <div class="interest_info pointer">Vue框架在ios微信端存在input输入时 系统键盘会将整个浏览器推上去</div>
+              <div class="interest_info pointer">小程序手机端数字键盘无法输入小数</div>
+              <div class="interest_info pointer">Java如何实现五分钟内重复获取返回 同一个短信验证码</div>
+            </div>
+          </div>
         </div>
         <!-- 评论     -->
         <div class="comment_container">
@@ -89,38 +97,66 @@
           <div class="comment_info bg-white">
             <div class="comment_text">
               <div class="current_user inline">
-                <img src="@/assets/img/image/code_presenter.png">
+                <img src="@/assets/img/image/code_presenter.png" />
               </div>
               <div class="current_edit inline">
                 <my-editor :height="'104px'" :placeholder="'我有一个大胆的想法～'"></my-editor>
+                <br />
+                <div class="text-right">
+                  <el-checkbox v-model="isAnonymous">匿名只是你穿的保护色～</el-checkbox>&emsp;&emsp;
+                  <el-button
+                    type="primary"
+                    round
+                    size="small"
+                    @click="submit()"
+                  >&emsp;评&nbsp;论&emsp;</el-button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-    </div>
-  </my-scrollbar>
+      </div>
+    </my-scrollbar>
+    <el-dialog :visible.sync="showDialog" :show-close="false" width="390px" center>
+      <template slot="title">
+        <h5 class="font-size--md text-dark line-height--lg">{{resultMsg}}</h5>
+      </template>
+      <div class="text-center">
+        <img :src="resultImage" class="my-img" />
+      </div>
+    </el-dialog>
+  </div>
 </template>
 <script>
 import * as custom from "@/assets/js/custom.config";
-import MyScrollbar from '@/components/scroller/Scrollbar';
-import MyEditor from '@/components/Editor';
-import { async } from 'q';
+import MyScrollbar from "@/components/scroller/Scrollbar";
+import MyEditor from "@/components/Editor";
+import { async } from "q";
+import successImg from "@/assets/img/image/image-system-submit-success.svg";
 export default {
   components: {
     MyScrollbar,
-    MyEditor,
+    MyEditor
   },
-  data () {
+  data() {
     return {
       config: custom.search,
-      list:  custom.search.list[0],
-    }
+      list: custom.search.list[0],
+      isAnonymous: false,
+      resultMsg: "",
+      resultImage: "",
+      showDialog: false
+    };
   },
-  mounted () {
-  },
+  mounted() {},
   methods: {
-  },
-}
+    submit() {
+      this.resultMsg = "发布成功!";
+      this.resultImage = successImg;
+      this.showDialog = true;
+    }
+  }
+};
 </script>
 <style lang='scss' scoped>
 .flt {
@@ -142,18 +178,18 @@ export default {
       width: 55px;
       height: 54px;
       img {
-        width: 100%
+        width: 100%;
       }
     }
     .support_text {
       padding: 12px 11px;
       font-size: 16px;
-      color: #B2BAC1;
+      color: #b2bac1;
       .separate {
         width: 26px;
         height: 1px;
         margin: 4px 2px;
-        background: #DCE5ED;
+        background: #dce5ed;
       }
     }
   }
@@ -168,123 +204,123 @@ export default {
       color: #000000;
       border-bottom: 1px solid #eff3f7;
       .detail_presenter {
-          margin-top: 17px;
-          font-size: 14px;
-          color: #394A58;
-          .presenter_head {
-            width: 30px;
-            height: 30px;
+        margin-top: 17px;
+        font-size: 14px;
+        color: #394a58;
+        .presenter_head {
+          width: 30px;
+          height: 30px;
+          img {
+            width: 100%;
+          }
+        }
+        .presenter_info {
+          height: 100%;
+          margin-left: 10px;
+          padding: 5px 0;
+        }
+        .mark_tags {
+          margin-left: 20px;
+          .mark_tag {
+            margin-right: 16px;
+            color: #9eadba;
+          }
+        }
+        .browse {
+          color: #9eadba;
+          .browse_icon {
+            width: 13px;
+            height: 9px;
             img {
               width: 100%;
             }
           }
-          .presenter_info {
-            height: 100%;
-            margin-left: 10px;
-            padding: 5px 0;
-          }
-          .mark_tags {
-            margin-left: 20px;
-            .mark_tag {
-              margin-right: 16px;
-              color:#9EADBA;
-            }
-          }
-          .browse {
-            color:#9EADBA;
-            .browse_icon {
-              width: 13px;
-              height: 9px;
-              img {
-                width: 100%;
-              }
-            }
-          }          
+        }
       }
     }
     .detail_content {
-        margin-top: 54px;
-        margin-bottom: 40px;
-        .detail_sign {
-            font-size:22px;
-            font-weight:600;
-            color: #000000;
-            .span_sign {
-                width: 10px;
-                height: 30px;
-                margin: 4px 14px 14px 0;
-                background: #3376FF;
-            }
+      margin-top: 54px;
+      margin-bottom: 40px;
+      .detail_sign {
+        font-size: 22px;
+        font-weight: 600;
+        color: #000000;
+        .span_sign {
+          width: 10px;
+          height: 30px;
+          margin: 4px 14px 14px 0;
+          background: #3376ff;
         }
-        .describe {
-          padding: 30px 52px;
-          .word {
-            padding: 0 65px 20px;
-            line-height: 35px;
-            font-size: 16px;
-            color: #000000;
-          }
-          .image {
-            img {
-              width: 100%;
-            }
+      }
+      .describe {
+        padding: 30px 52px;
+        .word {
+          padding: 0 65px 20px;
+          line-height: 35px;
+          font-size: 16px;
+          color: #000000;
+        }
+        .image {
+          img {
+            width: 100%;
           }
         }
+      }
     }
     .praise {
       margin: 0 57px;
       padding-top: 37px;
       padding-bottom: 41px;
-      border-top: 1px solid #EFF3F7;
+      border-top: 1px solid #eff3f7;
       .praise_img {
         width: 102px;
-        margin-left: calc((100% - 102px)/2); 
+        margin-left: calc((100% - 102px) / 2);
         img {
           width: 100%;
         }
       }
       .praise_num {
-        margin-left: calc((100% - 102px)/2); 
+        margin-left: calc((100% - 102px) / 2);
         padding-left: 13px;
         font-size: 18px;
-        color: #394A58;
+        color: #394a58;
       }
     }
   }
   .interest {
-      width: calc(25% - 26px);
-      .interest_title {
-          font-size: 18px;
-          font-weight: 600;
-          color: #000000;
-          .more {
-              padding-top: 4px;
-              font-size: 14px;
-              color: #3376FF;
-              text-decoration: none;
-          }
+    width: calc(25% - 26px);
+    .interest_title {
+      font-size: 18px;
+      font-weight: 600;
+      color: #000000;
+      .more {
+        padding-top: 4px;
+        font-size: 14px;
+        color: #3376ff;
+        text-decoration: none;
       }
-      .interest_list {
-          padding-top: 10px;
-          font-size: 16px;
-          color: #000000;          
-          .interest_info {
-              margin-bottom: 20px;
-              padding: 20px 33px 0 0;
-              border-top: 1px solid #EFF3F7;
-              text-overflow: ellipsis;
-              display: -webkit-box;
-              -webkit-box-orient: vertical;
-              -webkit-line-clamp: 2;
-              overflow: hidden;
-          }
-          .interest_info:first-child {
-              border-top: none;
-          }
-          .interest_info.active {
-              color: #3376FF;
-          }
+    }
+    .interest_list {
+      padding-top: 10px;
+      font-size: 16px;
+      color: #000000;
+      .interest_info {
+        margin-bottom: 20px;
+        padding: 20px 33px 0 0;
+        border-top: 1px solid #eff3f7;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
       }
+      .interest_info:first-child {
+        border-top: none;
+      }
+      .interest_info.active {
+        color: #3376ff;
+      }
+    }
   }
 }
 .comment_container {
