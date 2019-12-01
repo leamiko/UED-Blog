5308eda896e4121303d86ea14e4a393def48d8a9
chendezhen <chendezhen@zhongruigroup.com>
2019-12-01 18:03:29 +0800

    chore
    chore: 移除测试代码。

--- web-ued/pages/coding/index.vue
+++ web-ued/pages/coding/index.vue
@@ -170,15 +170,6 @@ export default {
     }
   },
   mounted() {
-    const loading = this.$loading({
-      lock: true,
-      text: 'Loading',
-      spinner: 'el-icon-loading',
-      background: 'rgba(0, 0, 0, 0.7)'
-    });
-    setTimeout(() => {
-      loading.close();
-    }, 2000);
     this.getHotData();
     this.getTags();
   }
