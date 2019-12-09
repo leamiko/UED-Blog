ab538730ada75a008ca4c5c6ace0c1e097ce44c9
sunshuai <sunshuai.1@zhongruigroup.com>
2019-12-09 14:33:18 +0800

    12。9上线

--- web-ued/pages/register.vue
+++ web-ued/pages/register.vue
@@ -139,8 +139,8 @@ export default {
         id: "wxBindQrcode",
         appid: process.env.WX_WEB_ID,
         scope: "snsapi_login",
-        redirect_uri: window.location.href,
-        // redirect_uri: 'http://ued.lunz.cn',
+        // redirect_uri: window.location.href,
+        redirect_uri: 'http://ued.lunz.cn',
         state: "",
         style: "",
         href: "data:text/css;base64,Ly8gLmltcG93ZXJCb3ggew0KLy8gICAucXJjb2RlIHsNCi8vICAgICB3aWR0aDogMTgwcHg7DQovLyAgICAgaGVpZ2h0OiAxODBweDsNCi8vICAgICBtYXJnaW4tdG9wOiAwcHg7DQovLyAgICAgbWFyZ2luLXJpZ2h0OiAxMThweDsNCi8vICAgICBib3JkZXI6IDFweCBzb2xpZCAjZTJlMmUyOw0KLy8gICB9DQovLyAgIC5pbmZvew0KLy8gICAgIGRpc3BsYXk6IG5vbmU7DQovLyAgIH0NCi8vICAgLnRpdGxlew0KLy8gICAgIGRpc3BsYXk6IG5vbmU7DQovLyAgIH0NCi8vIH0NCi5pbXBvd2VyQm94IC5xcmNvZGV7DQogIHdpZHRoOiAxODBweDsNCiAgaGVpZ2h0OiAxODBweDsNCiAgbWFyZ2luLXRvcDogMHB4Ow0KICBtYXJnaW4tcmlnaHQ6IDExOHB4Ow0KICBib3JkZXI6IDFweCBzb2xpZCAjZTJlMmUyOw0KfQ0KLmltcG93ZXJCb3ggLmluZm97DQogIGRpc3BsYXk6IG5vbmU7DQp9DQouaW1wb3dlckJveCAudGl0bGV7DQogIGRpc3BsYXk6IG5vbmU7DQp9"
