bfce9ebd850e6a3444f57c47ea82d307d4493854
Mahl <mahuili@zhongruigroup.com>
2020-01-10 14:05:41 +0800

    commit

--- web-ued/components/dialogs/PersonalInfo.vue
+++ web-ued/components/dialogs/PersonalInfo.vue
@@ -1,227 +1,214 @@
 <template>
-  <el-dialog :visible.sync="show"
-             width="610px"
-             :custom-class="classStyle"
-             :before-close="handleClose"
-             destroy-on-close
-             append-to-body>
-    <img src="@/assets/img/bg/bg-dialog-info.png"
-         slot="title"
-         class="dialog-img">
+  <el-dialog :visible.sync="show" width="610px" :custom-class="classStyle" :before-close="handleClose" destroy-on-close
+    append-to-body>
+    <img src="@/assets/img/bg/bg-dialog-info.png" slot="title" class="dialog-img">
     <div class="content_block">
       <p>昵称</p>
       <div class="cus-flex cus-align-center nickname">
-        <el-input v-model="name"
-                  placeholder="听说，好看的人都会给自己起一个与众不同的名字~"
-                  maxlength="20"></el-input>
+        <el-input v-model="name" placeholder="听说，好看的人都会给自己起一个与众不同的名字~" maxlength="20"></el-input>
         <span @click="generate">想不起名字</span>
       </div>
       <p>头像</p>
       <div class="cus-flex cus-align-center">
-        <div class="avator_list"
-             v-for="item in avatorList"
-             @click="chooseAvator(item)"
-             :key="item._id">
-          <el-avatar :size="50"
-                     :src="item.url"
-                     class="align-top"
-                     shape="square"
-                     :class="{'img_selected': item.isSelect}"></el-avatar>
-          <img class="selected"
-               src="../../assets/img/icon/avator-checked.png"
-               alt=""
-               v-if="item.isSelect">
+        <div class="avator_list" v-for="item in avatorList" @click="chooseAvator(item)" :key="item._id">
+          <el-avatar :size="50" :src="item.url" class="align-top" shape="square"
+            :class="{'img_selected': item.isSelect}"></el-avatar>
+          <img class="selected" src="../../assets/img/icon/avator-checked.png" alt="" v-if="item.isSelect">
         </div>
       </div>
     </div>
-    <div slot="footer"
-         class="dialog-footer">
-      <el-button type="primary"
-                 round
-                 size="large"
-                 @click="submit">确定</el-button>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="primary" round size="large" @click="submit">确定</el-button>
     </div>
   </el-dialog>
 
 </template>
 <style lang="scss" scoped>
-.info_dialog {
-  .nickname {
-    word-break: keep-all;
-    margin-bottom: 26px;
+  .info_dialog {
+    .nickname {
+      word-break: keep-all;
+      margin-bottom: 26px;
 
-    span {
-      color: #3376ff;
+      span {
+        color: #3376ff;
+        font-size: 16px;
+        border-bottom: 1px solid #3376ff;
+        line-height: 16px;
+        cursor: pointer;
+      }
+    }
+
+    .img_selected {
+      border-radius: 50%;
+      border: 3px solid #3376ff;
+      width: 54px !important;
+      height: 54px !important;
+      line-height: 54px !important;
+    }
+
+    .avator_list {
+      position: relative;
+
+      img.selected {
+        position: absolute;
+        bottom: 2px;
+        right: 16px;
+      }
+    }
+
+    p {
+      color: #000000;
       font-size: 16px;
-      border-bottom: 1px solid #3376ff;
-      line-height: 16px;
-      cursor: pointer;
+      font-weight: bold;
+      margin-bottom: 14px;
     }
   }
-
-  .img_selected {
-    border-radius: 50%;
-    border: 3px solid #3376ff;
-    width: 54px !important;
-    height: 54px !important;
-    line-height: 54px !important;
-  }
-
-  .avator_list {
-    position: relative;
-
-    img.selected {
-      position: absolute;
-      bottom: 2px;
-      right: 16px;
-    }
-  }
-
-  p {
-    color: #000000;
-    font-size: 16px;
-    font-weight: bold;
-    margin-bottom: 14px;
-  }
-}
 </style>
 <script>
-import * as custom from "@/assets/js/custom.config";
-export default {
-  props: {
-    isShow: {
-      default: false,
-      type: Boolean,
-      required: true
+  import * as custom from "@/assets/js/custom.config";
+  export default {
+    props: {
+      isShow: {
+        default: false,
+        type: Boolean,
+        required: true
+      },
+      classStyle: {
+        default: null,
+        type: String,
+        required: false
+      },
     },
-    classStyle: {
-      default: null,
-      type: String,
-      required: false
-    },
-  },
-  watch: {
-    isShow (newVal, oldVal) {
-      this.show = newVal;
-    },
-    "$store.state.flag": function () {
-      if (this.$store.state.flag !== null) {
-        this.memberInfo = JSON.parse(localStorage.getItem('user'))
-        this.name = this.memberInfo.nickName
-        this.imgUrl = this.memberInfo.avatar
-      }
-    }
-  },
-  data () {
-    return {
-      name: null,
-      show: false,
-      config: custom.write,
-      nameList: [],
-      memberInfo: {}, // 用户信息
-      imgUrl: null,
-      avatorList: [], // 头像列表
-    }
-  },
-  mounted () {
-    // if (localStorage.user) {
-    //     this.memberInfo = JSON.parse(localStorage.getItem('user'))
-    //     this.name = this.memberInfo.nickName
-    //     console.log(this.memberInfo);
-    // }
-    this.getAvatarList()
-  },
-  methods: {
-    handleClose (done) {
-      this.$emit('hide', true);
-      this.show = false;
-      this.name = this.memberInfo.nickName ? this.memberInfo.nickName : null
-      done();
-    },
-    // 获取头像
-    async getAvatarList () {
-      const res = await this.$axios.get(`${process.env.BASE_URL}/web_api/getAvatarList`);
-      if (res.status == 200) {
-        this.avatorList = res.data.data
-        this.avatorList.map(e => {
-          if (e.url === this.memberInfo.avatar) {
-            this.$set(e, 'isSelect', true)
-          } else {
-            this.$set(e, 'isSelect', false)
-          }
+    watch: {
+      isShow(newVal, oldVal) {
+        this.show = newVal;
+      },
+      "$store.state.flag": function () {
+        if (this.$store.state.flag !== null) {
+          this.memberInfo = JSON.parse(localStorage.getItem('user'))
+          this.name = this.memberInfo.nickName
+          this.imgUrl = this.memberInfo.avatar
+          this.avatorList.map(e => {
+            if (e.url === this.memberInfo.avatar) {
+              this.$set(e, 'isSelect', true)
+            } else {
+              this.$set(e, 'isSelect', false)
+            }
 
+          })
+        }
+      }
+    },
+    data() {
+      return {
+        name: null,
+        show: false,
+        config: custom.write,
+        nameList: [],
+        memberInfo: {}, // 用户信息
+        imgUrl: null,
+        avatorList: [], // 头像列表
+      }
+    },
+    mounted() {
+      // if (localStorage.user) {
+      //     this.memberInfo = JSON.parse(localStorage.getItem('user'))
+      //     this.name = this.memberInfo.nickName
+      //     console.log(this.memberInfo);
+      // }
+      this.getAvatarList()
+    },
+    methods: {
+      handleClose(done) {
+        this.$emit('hide', true);
+        this.show = false;
+        this.name = this.memberInfo.nickName ? this.memberInfo.nickName : null
+        done();
+      },
+      // 获取头像
+      async getAvatarList() {
+        const res = await this.$axios.get(`${process.env.BASE_URL}/web_api/getAvatarList`);
+        if (res.status == 200) {
+          this.avatorList = res.data.data
+          this.avatorList.map(e => {
+            if (e.url === this.memberInfo.avatar) {
+              this.$set(e, 'isSelect', true)
+            } else {
+              this.$set(e, 'isSelect', false)
+            }
+
+          })
+        }
+
+      },
+      // 生成随机昵称
+      async generate() {
+        const data = await this.$axios.get('/name.txt')
+        data.data = data.data.replace(/[\r\n]/g, " ")
+        this.nameList = data.data.split(' ')
+        this.nameList = this.nameList.filter(s => {
+          return s && s.trim()
         })
-      }
+        console.log(this.nameList)
+        this.name = this.nameList[Math.floor((Math.random() * this.nameList.length))]
+      },
+      // // 选择头像
+      chooseAvator(item) {
+        this.avatorList.forEach(e => {
+          e.isSelect = false
+        })
+        item.isSelect = true
+        this.imgUrl = item.url
+        // this.uploadImgToBase64(item.srcElement.currentSrc, (res) => {
+        //     this.imgUrl = res
+        //     console.log(this.imgUrl)
+        // })
+      },
+      uploadImgToBase64(img, callback) {
+        let image = new Image()
+        image.crossOrigin = '';
+        image.src = img;
+        image.onload = function () {
+          var canvas = document.createElement("canvas");
+          canvas.width = image.width;
+          canvas.height = image.height;
+          var ctx = canvas.getContext("2d");
+          ctx.drawImage(image, 0, 0, image.width, image.height);
+          var ext = image.src.substring(image.src.lastIndexOf(".") + 1).toLowerCase();
+          var dataURL = canvas.toDataURL("image/" + ext);
+          callback ? callback(dataURL) : null; //调用回调函数
+        }
+      },
 
-    },
-    // 生成随机昵称
-    async generate () {
-      const data = await this.$axios.get('/name.txt')
-      data.data = data.data.replace(/[\r\n]/g, " ")
-      this.nameList = data.data.split(' ')
-      this.nameList = this.nameList.filter(s => {
-        return s && s.trim()
-      })
-      console.log(this.nameList)
-      this.name = this.nameList[Math.floor((Math.random() * this.nameList.length))]
-    },
-    // // 选择头像
-    chooseAvator (item) {
-      this.avatorList.forEach(e => {
-        e.isSelect = false
-      })
-      item.isSelect = true
-      this.imgUrl = item.url
-      // this.uploadImgToBase64(item.srcElement.currentSrc, (res) => {
-      //     this.imgUrl = res
-      //     console.log(this.imgUrl)
-      // })
-    },
-    uploadImgToBase64 (img, callback) {
-      let image = new Image()
-      image.crossOrigin = '';
-      image.src = img;
-      image.onload = function () {
-        var canvas = document.createElement("canvas");
-        canvas.width = image.width;
-        canvas.height = image.height;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(image, 0, 0, image.width, image.height);
-        var ext = image.src.substring(image.src.lastIndexOf(".") + 1).toLowerCase();
-        var dataURL = canvas.toDataURL("image/" + ext);
-        callback ? callback(dataURL) : null; //调用回调函数
-      }
-    },
-
-    async submit () {
-      const params = {
-        _id: this.memberInfo._id,
-        nickName: this.name,
-        avatar: this.imgUrl
-      }
-      const res = await this.$axios.post(`${process.env.BASE_URL}/web_api/updateInfo`, params);
-      if (res.data.status_code == 200) {
-        let user = res.data.data
-        console.log(user)
-        if (user) {
-          localStorage.removeItem('user')
-          localStorage.setItem("user", JSON.stringify(user));
-          this.$emit('hide', true);
-          this.show = false;
-          this.$notify({
-            title: '成功',
-            message: '设置成功',
-            type: 'success'
+      async submit() {
+        const params = {
+          _id: this.memberInfo._id,
+          nickName: this.name,
+          avatar: this.imgUrl
+        }
+        const res = await this.$axios.post(`${process.env.BASE_URL}/web_api/updateInfo`, params);
+        if (res.data.status_code == 200) {
+          let user = res.data.data
+          console.log(user)
+          if (user) {
+            localStorage.removeItem('user')
+            localStorage.setItem("user", JSON.stringify(user));
+            this.$emit('hide', true);
+            this.show = false;
+            this.$notify({
+              title: '成功',
+              message: '设置成功',
+              type: 'success'
+            });
+          }
+          this.$store.commit("flag", new Date().toLocaleTimeString());
+        } else {
+          this.$notify.error({
+            title: '错误',
+            message: res.data.message
           });
         }
-        this.$store.commit("flag", new Date().toLocaleTimeString());
-      } else {
-        this.$notify.error({
-          title: '错误',
-          message: res.data.message
-        });
       }
     }
   }
-}
 </script>
